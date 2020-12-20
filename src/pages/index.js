515ab88f1bcbdfacfb96a8ca10c0f4be335fd009
sbdnd <sebastien.dunand@gmail.com>
2020-12-20 18:11:57 +0100

    HeroSection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeroSection from "../components/HeroSection"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home page</h1>
-    <p>What's goin ON</p>
+    <HeroSection />
   </Layout>
 )
 
